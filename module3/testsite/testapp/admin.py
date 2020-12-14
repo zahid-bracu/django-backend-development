c9bc40c3e2a1622289df23a1ac15840952702ae2
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-12-14 21:12:36 +0600

    update

--- module3/testsite/testapp/admin.py
+++ module3/testsite/testapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from testapp.models import Musician, Album
 
 # Register your models here.
+admin.site.register(Musician)
+admin.site.register(Album)
--- module3/testsite/testapp/models.py
+++ module3/testsite/testapp/models.py
@@ -12,9 +12,15 @@ class Musician(models.Model):
     last_name=models.CharField(max_length=50)
     instrument=models.CharField(max_length=100)
     
+def __str__(self):
+    return self.first_name+" "+self.last_name
 
 class Album(models.Model):
     artist=models.ForeignKey(Musician, on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     release_date=models.DateField()
     num_stars=models.IntegerField()
+
+
+
+
