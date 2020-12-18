afd52690d2335f4fde615acc9c81bf79cc54b79a
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-12-18 04:36:50 -0800

    up

--- module3/testsite/testapp/models.py
+++ module3/testsite/testapp/models.py
@@ -8,6 +8,7 @@ from django.db import models
 
 
 class Musician(models.Model):
+    id=models.AutoField(primary_key=True)
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
     instrument=models.CharField(max_length=100)
