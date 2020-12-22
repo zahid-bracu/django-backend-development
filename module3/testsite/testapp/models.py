4472421c4f65396887816e7238b1a7ef350d08de
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-12-21 21:21:55 -0800

    up

--- module3/testsite/testapp/models.py
+++ module3/testsite/testapp/models.py
@@ -9,18 +9,28 @@ from django.db import models
 
 class Musician(models.Model):
     id=models.AutoField(primary_key=True)
-    first_name=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50,blank=False)
     last_name=models.CharField(max_length=50)
     instrument=models.CharField(max_length=100)
-    
-def __str__(self):
-    return self.first_name+" "+self.last_name
+    def __str__(self):
+        return self.first_name+" "+self.last_name
 
 class Album(models.Model):
     artist=models.ForeignKey(Musician, on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     release_date=models.DateField()
-    num_stars=models.IntegerField()
+    
+    rating=(
+        (1,'worst'),
+        (2,'Bad'),
+        (3,'Not Bad'),
+        (4,'Good'),
+        (5,'Excellent')
+    )
+    
+    num_stars=models.IntegerField(choices=rating)
+    def __str__(self):
+        return self.name
 
 
 
